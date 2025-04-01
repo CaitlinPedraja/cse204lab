764f4df7ad29788c8a4cb61759b2f83cf7e4d618
caitlinpedraja <caitlinpedraja3@gmail.com>
2025-04-01 16:24:44 -0500

    add game functionality

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -75,3 +75,15 @@ function calculateWinner(squares) {
   }
   return null;
 }
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
