52dc0152b10d36f379a89d645275051234ed7c65
caitlinpedraja <caitlinpedraja3@gmail.com>
2025-04-01 16:26:02 -0500

    add moves storage

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -61,7 +61,19 @@ export default function Game() {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
-
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
