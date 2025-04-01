cc42cd541c61b0da6fb276f9ecac7695aa277c7a
caitlinpedraja <caitlinpedraja3@gmail.com>
2025-04-01 16:23:53 -0500

    update status

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -25,7 +25,7 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
-  
+
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -35,6 +35,7 @@ export default function Board() {
   }
   return (
     <>
+        <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
